ef0e1b0ab277b74b3f68ece3561e7e8d99204b35
meteorlxy <meteor.lxy@foxmail.com>
2021-05-20 21:26:40 +0800

    chore: add more cases

--- packages/@vuepress/markdown/__tests__/plugins/assetsPlugin.spec.ts
+++ packages/@vuepress/markdown/__tests__/plugins/assetsPlugin.spec.ts
@@ -3,20 +3,31 @@ import { assetsPlugin } from '@vuepress/markdown'
 import type { MarkdownEnv } from '@vuepress/markdown'
 
 const source = [
+  // relative paths
   '![foo](./foo.png)',
   '![foo2](../sub/foo.png)',
   '![foo-bar](./foo/bar.png)',
   '![foo-bar2](../sub/foo/bar.png)',
   '![baz](../baz.png)',
   '![out](../../out.png)',
-  '![invalid](.../invalid.png)',
+  '![汉字](./汉字.png)',
+  '![100%](./100%.png)',
+  // aliases
+  '![alias](@alias/foo.png)',
+  '![汉字](@alias/汉字.png)',
+  '![100%](@alias/100%.png)',
+  // webpack legacy aliases
+  '![~alias](~@alias/foo.png)',
+  '![~汉字](~@alias/汉字.png)',
+  '![~100%](~@alias/100%.png)',
+  // keep as is
   '![absolute](/absolute.png)',
   '![url](http://foobar.com/icon.png)',
   '![empty]()',
-  '![alias](@alias/foo.png)',
-  '![~alias](~@alias/foo.png)',
-  '![中国](./中国.png)',
-  '![finish](./100%.png)',
+  // invalid paths
+  '![invalid](.../invalid.png)',
+  '![汉字](.../汉字.png)',
+  '![100%](.../100%.png)',
 ].join('\n\n')
 
 describe('@vuepress/markdown > plugins > assetsPlugin', () => {
@@ -30,20 +41,31 @@ describe('@vuepress/markdown > plugins > assetsPlugin', () => {
 
     expect(rendered).toEqual(
       [
+        // relative paths
         '<img src="@source/sub/foo.png" alt="foo">',
         '<img src="@source/sub/foo.png" alt="foo2">',
         '<img src="@source/sub/foo/bar.png" alt="foo-bar">',
         '<img src="@source/sub/foo/bar.png" alt="foo-bar2">',
         '<img src="@source/baz.png" alt="baz">',
         '<img src="@source/../out.png" alt="out">',
-        '<img src=".../invalid.png" alt="invalid">',
+        '<img src="@source/sub/汉字.png" alt="汉字">',
+        '<img src="@source/sub/100%.png" alt="100%">',
+        // aliases
+        '<img src="@alias/foo.png" alt="alias">',
+        '<img src="@alias/汉字.png" alt="汉字">',
+        '<img src="@alias/100%.png" alt="100%">',
+        // webpack legacy aliases
+        '<img src="~@alias/foo.png" alt="~alias">',
+        '<img src="~@alias/汉字.png" alt="~汉字">',
+        '<img src="~@alias/100%.png" alt="~100%">',
+        // keep as is
         '<img src="/absolute.png" alt="absolute">',
         '<img src="http://foobar.com/icon.png" alt="url">',
         '<img src="" alt="empty">',
-        '<img src="@alias/foo.png" alt="alias">',
-        '<img src="~@alias/foo.png" alt="~alias">',
-        '<img src="@source/sub/中国.png" alt="中国">',
-        '<img src="@source/sub/100%.png" alt="finish">',
+        // invalid paths
+        '<img src=".../invalid.png" alt="invalid">',
+        '<img src=".../%E6%B1%89%E5%AD%97.png" alt="汉字">',
+        '<img src=".../100%25.png" alt="100%">',
       ]
         .map((item) => `<p>${item}</p>`)
         .join('\n') + '\n'
@@ -62,20 +84,31 @@ describe('@vuepress/markdown > plugins > assetsPlugin', () => {
 
     expect(rendered).toEqual(
       [
+        // relative paths
         '<img src="@foo/sub/foo.png" alt="foo">',
         '<img src="@foo/sub/foo.png" alt="foo2">',
         '<img src="@foo/sub/foo/bar.png" alt="foo-bar">',
         '<img src="@foo/sub/foo/bar.png" alt="foo-bar2">',
         '<img src="@foo/baz.png" alt="baz">',
         '<img src="@foo/../out.png" alt="out">',
-        '<img src=".../invalid.png" alt="invalid">',
+        '<img src="@foo/sub/汉字.png" alt="汉字">',
+        '<img src="@foo/sub/100%.png" alt="100%">',
+        // aliases
+        '<img src="@alias/foo.png" alt="alias">',
+        '<img src="@alias/汉字.png" alt="汉字">',
+        '<img src="@alias/100%.png" alt="100%">',
+        // webpack legacy aliases
+        '<img src="~@alias/foo.png" alt="~alias">',
+        '<img src="~@alias/汉字.png" alt="~汉字">',
+        '<img src="~@alias/100%.png" alt="~100%">',
+        // keep as is
         '<img src="/absolute.png" alt="absolute">',
         '<img src="http://foobar.com/icon.png" alt="url">',
         '<img src="" alt="empty">',
-        '<img src="@alias/foo.png" alt="alias">',
-        '<img src="~@alias/foo.png" alt="~alias">',
-        '<img src="@foo/sub/中国.png" alt="中国">',
-        '<img src="@foo/sub/100%.png" alt="finish">',
+        // invalid paths
+        '<img src=".../invalid.png" alt="invalid">',
+        '<img src=".../%E6%B1%89%E5%AD%97.png" alt="汉字">',
+        '<img src=".../100%25.png" alt="100%">',
       ]
         .map((item) => `<p>${item}</p>`)
         .join('\n') + '\n'
@@ -90,20 +123,31 @@ describe('@vuepress/markdown > plugins > assetsPlugin', () => {
 
     expect(rendered).toEqual(
       [
+        // relative paths
         '<img src="./foo.png" alt="foo">',
         '<img src="../sub/foo.png" alt="foo2">',
         '<img src="./foo/bar.png" alt="foo-bar">',
         '<img src="../sub/foo/bar.png" alt="foo-bar2">',
         '<img src="../baz.png" alt="baz">',
         '<img src="../../out.png" alt="out">',
-        '<img src=".../invalid.png" alt="invalid">',
+        '<img src="./%E6%B1%89%E5%AD%97.png" alt="汉字">',
+        '<img src="./100%25.png" alt="100%">',
+        // aliases
+        '<img src="@alias/foo.png" alt="alias">',
+        '<img src="@alias/汉字.png" alt="汉字">',
+        '<img src="@alias/100%.png" alt="100%">',
+        // webpack legacy aliases
+        '<img src="~@alias/foo.png" alt="~alias">',
+        '<img src="~@alias/汉字.png" alt="~汉字">',
+        '<img src="~@alias/100%.png" alt="~100%">',
+        // keep as is
         '<img src="/absolute.png" alt="absolute">',
         '<img src="http://foobar.com/icon.png" alt="url">',
         '<img src="" alt="empty">',
-        '<img src="@alias/foo.png" alt="alias">',
-        '<img src="~@alias/foo.png" alt="~alias">',
-        '<img src="./%E4%B8%AD%E5%9B%BD.png" alt="中国">',
-        '<img src="./100%25.png" alt="finish">',
+        // invalid paths
+        '<img src=".../invalid.png" alt="invalid">',
+        '<img src=".../%E6%B1%89%E5%AD%97.png" alt="汉字">',
+        '<img src=".../100%25.png" alt="100%">',
       ]
         .map((item) => `<p>${item}</p>`)
         .join('\n') + '\n'
