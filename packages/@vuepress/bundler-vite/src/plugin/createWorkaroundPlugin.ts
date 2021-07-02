7ba72e32fc2207afa8d4737889bd365bd0be1cf7
meteorlxy <meteor.lxy@foxmail.com>
2021-07-03 02:53:53 +0800

    chore(bundler-vite): tweak workaround plugin

--- packages/@vuepress/bundler-vite/src/plugin/createWorkaroundPlugin.ts
+++ packages/@vuepress/bundler-vite/src/plugin/createWorkaroundPlugin.ts
@@ -1,20 +1,25 @@
 import type { Plugin, ViteDevServer, DepOptimizationMetadata } from 'vite'
 
+/**
+ * Workaround for https://github.com/vitejs/vite/issues/2503
+ *
+ * Although the issue was closed in vite 2.3.0, there are still
+ * some problems with the version hash
+ */
 export const createWorkaroundPlugin = (): Plugin => {
   let server:
     | (ViteDevServer & { _optimizeDepsMetadata?: DepOptimizationMetadata })
     | null
   return {
     name: 'vuepress:workaround',
+
     enforce: 'pre',
+
     configureServer(_server) {
       server = _server
     },
+
     resolveId() {
-      // workaround for https://github.com/vitejs/vite/issues/2503
-      // although the issue was closed in vite 2.3.0, there are still
-      // some problems with the version hash and we need to keep the
-      // workaround for now
       if (server?._optimizeDepsMetadata?.browserHash) {
         server._optimizeDepsMetadata.browserHash = ''
       }
