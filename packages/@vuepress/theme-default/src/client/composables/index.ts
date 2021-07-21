e9744ad36c1419180529f429b7f02a5ed03691ef
meteorlxy <meteor.lxy@foxmail.com>
2021-07-21 11:04:36 +0800

    Update index.ts

--- packages/@vuepress/theme-default/src/client/composables/index.ts
+++ packages/@vuepress/theme-default/src/client/composables/index.ts
@@ -1,5 +1,5 @@
 export * from './useDarkMode'
-export * from './useHTMLClass'
+export * from './useHtmlDarkClass'
 export * from './useNavLink'
 export * from './usePrefersColorScheme'
 export * from './useResolveRouteWithRedirect'
