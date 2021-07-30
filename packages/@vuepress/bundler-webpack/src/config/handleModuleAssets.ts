a404feb52f654656e1f79ee7c56ee92d2dc15c48
meteorlxy <meteor.lxy@foxmail.com>
2021-07-31 00:20:44 +0800

    refactor(bundler-webpack): migrate to asset modules

--- packages/@vuepress/bundler-webpack/src/config/handleModuleAssets.ts
+++ packages/@vuepress/bundler-webpack/src/config/handleModuleAssets.ts
@@ -16,13 +16,10 @@ export const handleModuleAssets = ({
   // images
   config.module
     .rule('images')
-    .test(/\.(png|jpe?g|gif)(\?.*)?$/)
-    .use('url-loader')
-    .loader('url-loader')
-    .options({
-      limit: inlineLimit,
-      // always add hash to avoid name conflicts
-      name: 'assets/img/[name].[contenthash:8].[ext]',
+    .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+    .type('asset' as any)
+    .set('generator', {
+      filename: 'assets/img/[name].[contenthash:8][ext]',
     })
 
   // svg
@@ -31,34 +28,26 @@ export const handleModuleAssets = ({
   config.module
     .rule('svg')
     .test(/\.(svg)(\?.*)?$/)
-    .use('file-loader')
-    .loader('file-loader')
-    .options({
-      // always add hash to avoid name conflicts
-      name: 'assets/img/[name].[contenthash:8].[ext]',
+    .type('asset/resource' as any)
+    .set('generator', {
+      filename: 'assets/img/[name].[contenthash:8][ext]',
     })
 
   // media
   config.module
     .rule('media')
     .test(/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
-    .use('url-loader')
-    .loader('url-loader')
-    .options({
-      limit: inlineLimit,
-      // always add hash to avoid name conflicts
-      name: 'assets/media/[name].[contenthash:8].[ext]',
+    .type('asset/resource' as any)
+    .set('generator', {
+      filename: 'assets/media/[name].[contenthash:8][ext]',
     })
 
   // fonts
   config.module
     .rule('fonts')
     .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
-    .use('url-loader')
-    .loader('url-loader')
-    .options({
-      limit: inlineLimit,
-      // always add hash to avoid name conflicts
-      name: 'assets/fonts/[name].[contenthash:8].[ext]',
+    .type('asset/resource' as any)
+    .set('generator', {
+      filename: 'assets/fonts/[name].[contenthash:8][ext]',
     })
 }
