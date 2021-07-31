d0927e3feb8e25d6d2aa869d6013537ce5ba1c2a
lyh <1347635797@qq.com>
2021-07-31 10:18:03 +0800

    change preparePageComponent function

--- packages/@vuepress/core/src/app/prepare/preparePageComponent.ts
+++ packages/@vuepress/core/src/app/prepare/preparePageComponent.ts
@@ -7,11 +7,15 @@ export const preparePageComponent = async (
   app: App,
   page: Page
 ): Promise<void> => {
+  let  contentRendered = `<template>${page.contentRendered}</template>\n`;
+  if(page.contentRendered.indexOf("<template>") === 0 && page.contentRendered.indexOf("</template>") !== -1){
+    contentRendered  = page.contentRendered
+  }
   await app.writeTemp(
     page.componentFilePathRelative,
     [
       // take the rendered markdown content as <template>
-      `<template>${page.contentRendered}</template>\n`,
+      contentRendered,
       // hoist `<script>`, `<style>` and other custom blocks
       ...page.hoistedTags,
     ].join('\n')
