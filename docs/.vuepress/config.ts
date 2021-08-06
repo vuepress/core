2ef9f1815a1235a80f31a080e91cbbf3c6f32bd3
JiaZeng <86162890+jiazengp@users.noreply.github.com>
2021-08-07 02:00:49 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -131,7 +131,7 @@ export default defineUserConfig<DefaultThemeOptions>({
         // a11y
         openInNewWindow: '在新窗口打开',
         toggleDarkMode: '切换夜间模式',
-        toggleSidebar: '切换侧边栏'
+        toggleSidebar: '切换侧边栏',
       },
     },
 
