95874df8d9fa850b7a9c31862e49a75f146784c8
meteorlxy <meteor.lxy@foxmail.com>
2021-08-12 19:23:31 +0800

    refactor(theme-default): extract setupSidebarItems from clientAppSetup

--- packages/@vuepress/theme-default/src/client/clientAppSetup.ts
+++ packages/@vuepress/theme-default/src/client/clientAppSetup.ts
@@ -1,19 +1,6 @@
-import { computed, provide } from 'vue'
-import { defineClientAppSetup, usePageFrontmatter } from '@vuepress/client'
-import {
-  resolveSidebarItems,
-  sidebarItemsSymbol,
-  useThemeLocaleData,
-} from './composables'
-import type { DefaultThemeNormalPageFrontmatter } from '../shared'
+import { defineClientAppSetup } from '@vuepress/client'
+import { setupSidebarItems } from './composables'
 
 export default defineClientAppSetup(() => {
-  // we need to access sidebar items in multiple components
-  // so we make it global computed
-  const themeLocale = useThemeLocaleData()
-  const frontmatter = usePageFrontmatter<DefaultThemeNormalPageFrontmatter>()
-  const sidebarItems = computed(() =>
-    resolveSidebarItems(frontmatter.value, themeLocale.value)
-  )
-  provide(sidebarItemsSymbol, sidebarItems)
+  setupSidebarItems()
 })
