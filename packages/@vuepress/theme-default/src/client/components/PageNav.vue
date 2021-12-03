e7b31fdad9d6cc410b332458800aa1e00d538643
gcxfd <i@rmw.link>
2021-12-04 01:52:32 +0800

    feat(theme-default): make the arrows in page nav clickable (#540)

--- packages/@vuepress/theme-default/src/client/components/PageNav.vue
+++ packages/@vuepress/theme-default/src/client/components/PageNav.vue
@@ -2,13 +2,11 @@
   <nav v-if="prevNavLink || nextNavLink" class="page-nav">
     <p class="inner">
       <span v-if="prevNavLink" class="prev">
-        ←
         <NavLink :item="prevNavLink" />
       </span>
 
       <span v-if="nextNavLink" class="next">
         <NavLink :item="nextNavLink" />
-        →
       </span>
     </p>
   </nav>
