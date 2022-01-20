56788930264394edbb7f03743f54bddb555aa403
meteorlxy <meteor.lxy@foxmail.com>
2022-01-20 15:24:34 +0800

    chore: disable prismjs in prod mode

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -138,6 +138,8 @@ export default defineUserConfig<DefaultThemeOptions>({
     themePlugins: {
       // only enable git plugin in production mode
       git: isProd,
+      // use shiki plugin in production mode instead
+      prismjs: !isProd,
     },
   },
 
