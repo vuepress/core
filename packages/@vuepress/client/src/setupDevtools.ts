a19d945445a4b50455553c1221eaeefda6e73211
meteorlxy <meteor.lxy@foxmail.com>
2022-01-20 17:38:37 +0800

    feat(client): add vue-devtools support

---
+++ packages/@vuepress/client/src/setupDevtools.ts
@@ -0,0 +1,32 @@
+import { setupDevtoolsPlugin } from '@vue/devtools-api'
+import type { App } from 'vue'
+import type { GlobalComputed } from './provideGlobalComputed'
+
+export const setupDevtools = (
+  app: App,
+  globalComputed: GlobalComputed
+): void => {
+  setupDevtoolsPlugin(
+    {
+      app,
+      id: 'org.vuejs.vuepress',
+      label: 'VuePress',
+      packageName: '@vuepress/client',
+      homepage: 'https://v2.vuepress.vuejs.org',
+      logo: 'https://v2.vuepress.vuejs.org/images/hero.png',
+      componentStateTypes: ['VuePress'],
+    },
+    (api) => {
+      api.on.inspectComponent((payload) => {
+        payload.instanceData.state.push(
+          ...Object.entries(globalComputed).map(([name, item]) => ({
+            type: 'VuePress',
+            key: name,
+            editable: false,
+            value: item.value,
+          }))
+        )
+      })
+    }
+  )
+}
--- packages/@vuepress/client/types.d.ts
+++ packages/@vuepress/client/types.d.ts
@@ -2,3 +2,5 @@ declare const __VUEPRESS_VERSION__: string
 declare const __VUEPRESS_DEV__: boolean
 declare const __VUEPRESS_SSR__: boolean
 declare const __VUE_HMR_RUNTIME__: Record<string, any>
+declare const __VUE_OPTIONS_API__: boolean
+declare const __VUE_PROD_DEVTOOLS__: boolean
