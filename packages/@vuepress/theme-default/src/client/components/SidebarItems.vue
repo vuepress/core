d486caf15e70819eddcc5efb4abb8b91aac0d9a7
zh-lx <18366276315@163.com>
2022-02-17 23:57:50 +0800

    feat(theme-default): subtitle automatically appears in the view

--- packages/@vuepress/theme-default/src/client/components/SidebarItems.vue
+++ packages/@vuepress/theme-default/src/client/components/SidebarItems.vue
@@ -1,8 +1,43 @@
 <script setup lang="ts">
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
 import SidebarItem from '@theme/SidebarItem.vue'
 import { useSidebarItems } from '../composables'
 
 const sidebarItems = useSidebarItems()
+
+const route = useRouter();
+
+watch(
+  () => route,
+  (router) => {
+    // the sidebarItem dom whose href equals to the current href of the page
+    const activeSidebarItem = document.querySelector(
+      `[href="${router.currentRoute.value.path}${router.currentRoute.value.hash}"]`
+    );
+
+    const sidebar = document.querySelector('.sidebar')
+    if (activeSidebarItem && sidebar) {
+      // the top and height of the sidebarItem
+      const activeSidebarItemTop = activeSidebarItem.getBoundingClientRect().top
+      const activeSidebarItemHeight = activeSidebarItem.getBoundingClientRect().height
+      // the top and height of the sidebar
+      const sidebarTop = sidebar.getBoundingClientRect().top
+      const sidebarHeight = sidebar.getBoundingClientRect().height
+
+      if (activeSidebarItemTop < sidebarTop) {
+        // scroll to the top edge of sidebar when sidebarItem overflows the top edge of sidebar
+        activeSidebarItem.scrollIntoView(true)
+      } else if ( activeSidebarItemTop + activeSidebarItemHeight > sidebarTop + sidebarHeight) {
+        // scroll to the bottom edge of sidebar when sidebarItem overflows the bottom edge of sidebar
+        activeSidebarItem.scrollIntoView(false);
+      }
+    }
+  },
+  {
+    deep: true,
+  }
+);
 </script>
 
 <template>
