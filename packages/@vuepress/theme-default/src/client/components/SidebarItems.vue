29aeb6a4147f3bb4da9730b447cec04c1ab28ebe
zhoulixiang <18366276315@163.com>
2022-02-18 10:19:37 +0800

    feat(theme-default): subtitle automatically appears in the view when it overflows the visual area
    change code styles

--- packages/@vuepress/theme-default/src/client/components/SidebarItems.vue
+++ packages/@vuepress/theme-default/src/client/components/SidebarItems.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+import SidebarItem from '@theme/SidebarItem.vue'
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
-import SidebarItem from '@theme/SidebarItem.vue'
 import { useSidebarItems } from '../composables'
 
 const sidebarItems = useSidebarItems()
 
-const route = useRouter();
+const route = useRouter()
 
 watch(
   () => route,
@@ -14,13 +14,14 @@ watch(
     // the sidebarItem dom whose href equals to the current href of the page
     const activeSidebarItem = document.querySelector(
       `[href="${router.currentRoute.value.path}${router.currentRoute.value.hash}"]`
-    );
+    )
 
     const sidebar = document.querySelector('.sidebar')
     if (activeSidebarItem && sidebar) {
       // the top and height of the sidebarItem
       const activeSidebarItemTop = activeSidebarItem.getBoundingClientRect().top
       const activeSidebarItemHeight = activeSidebarItem.getBoundingClientRect().height
+      
       // the top and height of the sidebar
       const sidebarTop = sidebar.getBoundingClientRect().top
       const sidebarHeight = sidebar.getBoundingClientRect().height
@@ -28,16 +29,19 @@ watch(
       if (activeSidebarItemTop < sidebarTop) {
         // scroll to the top edge of sidebar when sidebarItem overflows the top edge of sidebar
         activeSidebarItem.scrollIntoView(true)
-      } else if ( activeSidebarItemTop + activeSidebarItemHeight > sidebarTop + sidebarHeight) {
+      } else if (
+        activeSidebarItemTop + activeSidebarItemHeight >
+        sidebarTop + sidebarHeight
+      ) {
         // scroll to the bottom edge of sidebar when sidebarItem overflows the bottom edge of sidebar
-        activeSidebarItem.scrollIntoView(false);
+        activeSidebarItem.scrollIntoView(false)
       }
     }
   },
   {
     deep: true,
   }
-);
+)
 </script>
 
 <template>
