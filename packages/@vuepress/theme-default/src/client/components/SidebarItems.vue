2d9ddac990aa02c49e449b69c4ddd8c1415cb247
zh-lx <18366276315@163.com>
2022-02-24 22:43:15 +0800

    feat: optimize code

--- packages/@vuepress/theme-default/src/client/components/SidebarItems.vue
+++ packages/@vuepress/theme-default/src/client/components/SidebarItems.vue
@@ -6,21 +6,22 @@ import { useSidebarItems } from '../composables'
 
 const sidebarItems = useSidebarItems()
 
-const route = useRouter();
+const router = useRouter()
 
 watch(
-  () => route,
-  (router) => {
+  () => router.currentRoute.value.hash,
+  (hash) => {
     // the sidebarItem dom whose href equals to the current href of the page
     const activeSidebarItem = document.querySelector(
-      `[href="${router.currentRoute.value.path}${router.currentRoute.value.hash}"]`
-    );
+      `.sidebar a.sidebar-item[href="${router.currentRoute.value.path}${hash}"]`
+    )
 
     const sidebar = document.querySelector('.sidebar')
     if (activeSidebarItem && sidebar) {
       // the top and height of the sidebarItem
       const activeSidebarItemTop = activeSidebarItem.getBoundingClientRect().top
-      const activeSidebarItemHeight = activeSidebarItem.getBoundingClientRect().height
+      const activeSidebarItemHeight =
+        activeSidebarItem.getBoundingClientRect().height
       // the top and height of the sidebar
       const sidebarTop = sidebar.getBoundingClientRect().top
       const sidebarHeight = sidebar.getBoundingClientRect().height
@@ -28,16 +29,16 @@ watch(
       if (activeSidebarItemTop < sidebarTop) {
         // scroll to the top edge of sidebar when sidebarItem overflows the top edge of sidebar
         activeSidebarItem.scrollIntoView(true)
-      } else if ( activeSidebarItemTop + activeSidebarItemHeight > sidebarTop + sidebarHeight) {
+      } else if (
+        activeSidebarItemTop + activeSidebarItemHeight >
+        sidebarTop + sidebarHeight
+      ) {
         // scroll to the bottom edge of sidebar when sidebarItem overflows the bottom edge of sidebar
-        activeSidebarItem.scrollIntoView(false);
+        activeSidebarItem.scrollIntoView(false)
       }
     }
-  },
-  {
-    deep: true,
   }
-);
+)
 </script>
 
 <template>
