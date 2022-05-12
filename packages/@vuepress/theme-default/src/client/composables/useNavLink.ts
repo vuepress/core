dd1d2408ba67519f6bfaa1dd55772d80d894f5ac
meteorlxy <meteor.lxy@foxmail.com>
2022-05-12 18:12:31 +0800

    fix(theme-default): allow non-url-encoded filename in theme config (close #884)

--- packages/@vuepress/theme-default/src/client/composables/useNavLink.ts
+++ packages/@vuepress/theme-default/src/client/composables/useNavLink.ts
@@ -15,7 +15,10 @@ declare module 'vue-router' {
  * - Output: { text: 'Home', link: '/' }
  */
 export const useNavLink = (item: string): NavLink => {
-  const resolved = useResolveRouteWithRedirect(item)
+  // the route path of vue-router is url-encoded, and we expect users are using
+  // non-url-encoded string in theme config, so we need to url-encode it first to
+  // resolve the route correctly
+  const resolved = useResolveRouteWithRedirect(encodeURI(item))
   return {
     text: resolved.meta.title || item,
     link: resolved.name === '404' ? item : resolved.fullPath,
