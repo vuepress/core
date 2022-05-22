964c30851f2458f0704526f2633ed0e7e78b0888
meteorlxy <meteor.lxy@foxmail.com>
2022-05-22 11:44:38 +0800

    feat(plugin-external-link-icon): add frontmatter type

--- packages/@vuepress/plugin-external-link-icon/src/shared/types.ts
+++ packages/@vuepress/plugin-external-link-icon/src/shared/types.ts
@@ -3,3 +3,7 @@ import type { LocaleConfig } from '@vuepress/shared'
 export type ExternalLinkIconLocales = LocaleConfig<{
   openInNewWindow: string
 }>
+
+export interface ExternalLinkIconFrontmatter {
+  externalLinkIcon?: boolean
+}
