be02e140d838c8fc1d8e1237df65a884c9e11b3d
meteorlxy <meteor.lxy@foxmail.com>
2022-05-28 12:05:54 +0800

    chore: tweaks

--- packages/@vuepress/plugin-search/src/client/utils/isKeyMatched.ts
+++ packages/@vuepress/plugin-search/src/client/utils/isKeyMatched.ts
@@ -1,3 +1,4 @@
+import { isString } from '@vuepress/shared'
 import type { HotKeyOptions } from '../../shared'
 
 export const isKeyMatched = (
@@ -5,16 +6,15 @@ export const isKeyMatched = (
   hotKeys: (string | HotKeyOptions)[]
 ): boolean =>
   hotKeys.some((hotKey) => {
-    if (typeof hotKey === 'object') {
-      const { key, ctrl = false, shift = false, alt = false } = hotKey
-
-      return (
-        key === event.key &&
-        ctrl === event.ctrlKey &&
-        shift === event.shiftKey &&
-        alt === event.altKey
-      )
+    if (isString(hotKey)) {
+      return hotKey === event.key
     }
 
-    return hotKey === event.key
+    const { key, ctrl = false, shift = false, alt = false } = hotKey
+    return (
+      key === event.key &&
+      ctrl === event.ctrlKey &&
+      shift === event.shiftKey &&
+      alt === event.altKey
+    )
   })
