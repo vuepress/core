faf96bc1af8d392e477c4fdc9038d4a2a1cdc8dc
meteorlxy <meteor.lxy@foxmail.com>
2022-07-12 00:43:53 +0800

    build: bump dependencies

--- packages/@vuepress/plugin-docsearch/src/client/components/Docsearch.ts
+++ packages/@vuepress/plugin-docsearch/src/client/components/Docsearch.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error: docsearch type issue
 import docsearch from '@docsearch/js'
 import { usePageLang, useRouteLocale } from '@vuepress/client'
 import { isArray } from '@vuepress/shared'
