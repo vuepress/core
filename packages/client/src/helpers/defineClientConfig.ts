e418e02b8c6bb179fd2aae2ce1abb9341038e2d0
meteorlxy <meteor.lxy@foxmail.com>
2022-08-24 23:28:00 +0800

    feat(client): make args of defineClientConfig optional

--- packages/client/src/helpers/defineClientConfig.ts
+++ packages/client/src/helpers/defineClientConfig.ts
@@ -3,5 +3,6 @@ import type { ClientConfig } from '../types/index.js'
 /**
  * A helper function to help you define vuepress client config file
  */
-export const defineClientConfig = (clientConfig: ClientConfig): ClientConfig =>
-  clientConfig
+export const defineClientConfig = (
+  clientConfig: ClientConfig = {}
+): ClientConfig => clientConfig
