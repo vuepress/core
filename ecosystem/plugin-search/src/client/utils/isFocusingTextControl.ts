bfe71f74e4570a1e98655fa820d426cabd6b066d
meteorlxy <meteor.lxy@foxmail.com>
2022-10-21 23:38:05 +0800

    Update isFocusingTextControl.ts

--- ecosystem/plugin-search/src/client/utils/isFocusingTextControl.ts
+++ ecosystem/plugin-search/src/client/utils/isFocusingTextControl.ts
@@ -1,14 +1,16 @@
 /**
- * Determines whether the user is currently focusing a text control. In this case, the search plugin shouldn’t hijack any hotkeys because the user might be typing into a text field, using type-ahead search in a `select` element, etc.
+ * Determines whether the user is currently focusing a text control.
+ * In this case, the search plugin shouldn’t hijack any hotkeys because
+ * the user might be typing into a text field, using type-ahead search 
+ * in a `select` element, etc.
  */
-export function isFocusingTextControl(target: EventTarget): boolean {
-  if (target instanceof Element) {
-    return (
-      document.activeElement === target &&
-      (['TEXTAREA', 'SELECT', 'INPUT'].includes(target.tagName) ||
-        target.hasAttribute('contenteditable'))
-    )
+export const isFocusingTextControl = (target: EventTarget): boolean => {
+  if (!(target instanceof Element)) {
+    return false
   }
-
-  return false
+  return (
+    document.activeElement === target &&
+    (['TEXTAREA', 'SELECT', 'INPUT'].includes(target.tagName) ||
+      target.hasAttribute('contenteditable'))
+  )
 }
