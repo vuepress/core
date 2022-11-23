7ff644bc1cab78a2435b34c5b88d4597fe4a82c6
Mr.Hope <mister-hope@outlook.com>
2022-11-23 13:53:08 +0800

    Update mainPlugin.ts

--- packages/bundler-vite/src/plugins/mainPlugin.ts
+++ packages/bundler-vite/src/plugins/mainPlugin.ts
@@ -52,9 +52,8 @@ export const mainPlugin = ({
   name: 'vuepress:main',
 
   config: async () => {
-  
     const cache = new Map<string, boolean>()
-  
+
     // create a temp index.html as dev entry point
     if (!isBuild) {
       await app.writeTemp(
