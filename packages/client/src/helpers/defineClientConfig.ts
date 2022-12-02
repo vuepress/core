00585153377533320b8a4362a218f21b7b987eea
dong <2710732812@qq.com>
2022-12-03 00:19:35 +0800

    fix(client): fix defineClientConfig will get undefined in HMR

--- packages/client/src/helpers/defineClientConfig.ts
+++ packages/client/src/helpers/defineClientConfig.ts
@@ -3,6 +3,8 @@ import type { ClientConfig } from '../types/index.js'
 /**
  * A helper function to help you define vuepress client config file
  */
-export const defineClientConfig = (
+export function defineClientConfig(
   clientConfig: ClientConfig = {}
-): ClientConfig => clientConfig
+): ClientConfig {
+  return clientConfig
+}
