wtf1998/vuepress-next

feat(markdown): support relative image link without `./`

packages/markdown/src/plugins/assetsPlugin/resolveLink.ts
@@ -5,14 +5,22 @@ import type { MarkdownEnv } from '../../types.js'
 export const resolveLink = (
   link: string,
   relativePathPrefix: string,
-  env: MarkdownEnv
+  env: MarkdownEnv,
+  strict = false
 ): string => {
   // decode link to ensure bundler can find the file correctly
   let resolvedLink = decode(link)
 
   // if the link is relative path, and the `env.filePathRelative` exists
   // add `@source` alias to the link
-  if (/^\.{1,2}\//.test(link) && env.filePathRelative) {
+  if (
+    (strict
+      ? // only link starts with `./` or `../`
+        /^\.{1,2}\//.test(link)
+      : // link without protocal and not absolute links
+        !link.startsWith('/') && !/[A-z]+:\/\//.test(link)) &&
+    env.filePathRelative
+  ) {
     resolvedLink = `${relativePathPrefix}/${path.join(
       path.dirname(env.filePathRelative),
       resolvedLink

vuepress/core

packages/markdown/src/plugins/assetsPlugin/resolveLink.ts
@@ -5,14 +5,22 @@ import type { MarkdownEnv } from '../../types.js'
 export const resolveLink = (
   link: string,
   relativePathPrefix: string,
-  env: MarkdownEnv
+  env: MarkdownEnv,
+  strict = false
 ): string => {
   // decode link to ensure bundler can find the file correctly
   let resolvedLink = decode(link)
 
   // if the link is relative path, and the `env.filePathRelative` exists
   // add `@source` alias to the link
-  if (/^\.{1,2}\//.test(link) && env.filePathRelative) {
+  if (
+    (strict
+      ? // only link starts with `./` or `../`
+        /^\.{1,2}\//.test(link)
+      : // link without protocol and not absolute links
+        !link.startsWith('/') && !/[A-z]+:\/\//.test(link)) &&
+    env.filePathRelative
+  ) {
     resolvedLink = `${relativePathPrefix}/${path.join(
       path.dirname(env.filePathRelative),
       resolvedLink