cf88b6235640e43f77b04ff4201e8955a589032d
meteorlxy <meteor.lxy@foxmail.com>
2022-12-14 12:11:33 +0800

    refactor(core): remove temp file memory cache

--- packages/core/src/app/resolveAppWriteTemp.ts
+++ packages/core/src/app/resolveAppWriteTemp.ts
@@ -5,17 +5,10 @@ import type { AppDir, AppWriteTemp } from '../types/index.js'
  * Resolve write temp file util for vuepress app
  */
 export const resolveAppWriteTemp = (dir: AppDir): AppWriteTemp => {
-  const tempCache = new Map<string, string>()
-
   const writeTemp: AppWriteTemp = async (file: string, content: string) => {
     const filePath = dir.temp(file)
-    const contentCached = tempCache.get(filePath)
-    if (contentCached !== content) {
-      await fs.outputFile(filePath, content)
-      tempCache.set(filePath, content)
-    }
+    await fs.outputFile(filePath, content)
     return filePath
   }
-
   return writeTemp
 }
