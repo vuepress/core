23a4c681cfd50607f396cb3894ede3cc60698d1b
Mr.Hope <mister-hope@outlook.com>
2023-01-16 17:11:33 +0800

    feat(plugin-docsearch): support indexBase option (close #1223) (#1224)
    Co-authored-by: meteorlxy <meteor.lxy@foxmail.com>

--- ecosystem/plugin-docsearch/src/node/docsearchPlugin.ts
+++ ecosystem/plugin-docsearch/src/node/docsearchPlugin.ts
@@ -4,20 +4,33 @@ import type { DocsearchOptions } from '../shared/index.js'
 
 const __dirname = getDirname(import.meta.url)
 
+/**
+ * Options for @vuepress/plugin-docsearch
+ */
 export interface DocsearchPluginOptions extends DocsearchOptions {
+  /**
+   * Base path of the search index
+   */
+  indexBase?: string
+
+  /**
+   * Whether to inject docsearch default styles
+   */
   injectStyles?: boolean
 }
 
 export const docsearchPlugin = ({
   injectStyles = true,
+  indexBase,
   ...options
 }: DocsearchPluginOptions): Plugin => ({
   name: '@vuepress/plugin-docsearch',
 
   clientConfigFile: path.resolve(__dirname, '../client/config.js'),
 
-  define: {
-    __DOCSEARCH_OPTIONS__: options,
+  define: (app) => ({
     __DOCSEARCH_INJECT_STYLES__: injectStyles,
-  },
+    __DOCSEARCH_INDEX_BASE__: indexBase || app.options.base,
+    __DOCSEARCH_OPTIONS__: options,
+  }),
 })
