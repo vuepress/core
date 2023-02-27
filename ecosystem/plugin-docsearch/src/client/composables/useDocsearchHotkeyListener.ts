e9b35cf51f55806963c34d697e707aed791430dd
Mr.Hope <mister-hope@outlook.com>
2023-02-27 20:16:49 +0800

    refactor(plugin-docsearch): introduce vueuse package (#1271)

--- ecosystem/plugin-docsearch/src/client/composables/useDocsearchHotkeyListener.ts
+++ ecosystem/plugin-docsearch/src/client/composables/useDocsearchHotkeyListener.ts
@@ -1,16 +1,14 @@
-import { onMounted, onUnmounted } from 'vue'
+import { useEventListener } from '@vueuse/core'
 
 /**
  * Add hotkey listener, remove it after triggered
  */
 export const useDocsearchHotkeyListener = (callback: () => void): void => {
-  const hotkeyListener = (event: KeyboardEvent): void => {
+  const remove = useEventListener('keydown', (event) => {
     if (event.key === 'k' && (event.ctrlKey || event.metaKey)) {
       event.preventDefault()
-      window.removeEventListener('keydown', hotkeyListener)
       callback()
+      remove()
     }
-  }
-  onMounted(() => window.addEventListener('keydown', hotkeyListener))
-  onUnmounted(() => window.removeEventListener('keydown', hotkeyListener))
+  })
 }
