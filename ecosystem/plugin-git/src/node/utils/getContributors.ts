c38f70395c70492356b54d11285d49b41482e5ca
Xinyu Liu <meteor.lxy@foxmail.com>
2023-07-11 19:36:24 +0800

    Update getContributors.ts

--- ecosystem/plugin-git/src/node/utils/getContributors.ts
+++ ecosystem/plugin-git/src/node/utils/getContributors.ts
@@ -24,13 +24,13 @@ export const getContributors = async (
       commits: Number.parseInt(commits, 10),
     }))
     .filter((item, index, self) => {
-      // If one of the contributors is a "noreply" email address, *and* there's
-      // already a contributor *with the same name*, it is very likely a duplicate,
+      // If one of the contributors is a "noreply" email address, and there's
+      // already a contributor with the same name, it is very likely a duplicate,
       // so it can be removed.
       if (item.email.split('@')[1]?.match(/no-?reply/)) {
         const realIndex = self.findIndex((t) => t.name === item.name)
         if (realIndex !== index) {
-          // Before filtering, update the "real" contributor to also include the noreply's commits
+          // Update the "real" contributor to also include the noreply's commits
           self[realIndex].commits += item.commits
           return false
         }
