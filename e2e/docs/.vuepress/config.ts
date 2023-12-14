2fe35bb3a358b59d2b14b4bcae7eb644768e80ba
meteorlxy <meteor.lxy@foxmail.com>
2023-12-14 15:18:01 +0800

    fix(client): merge locales head correctly

--- e2e/docs/.vuepress/config.ts
+++ e2e/docs/.vuepress/config.ts
@@ -7,20 +7,31 @@ import { e2eTheme } from './theme/node/e2eTheme.js'
 export default defineUserConfig({
   base: '/',
 
-  head: [],
+  head: [
+    ['meta', { name: 'foo', content: 'foo' }],
+    ['meta', { name: 'bar', content: 'bar' }],
+    ['meta', { name: 'baz', content: 'baz' }],
+  ],
 
   locales: {
     '/': {
       lang: 'en-US',
       title: 'VuePress E2E',
       description: 'VuePress E2E Test Site',
-      head: [],
+      head: [
+        ['meta', { name: 'foo-en', content: 'foo-en' }],
+        ['meta', { name: 'bar', content: 'foobar' }],
+        ['meta', { name: 'baz', content: 'foobar baz' }],
+      ],
     },
     '/zh/': {
       lang: 'zh-CN',
       title: 'VuePress E2E',
       description: 'VuePress E2E 测试站点',
-      head: [],
+      head: [
+        ['meta', { name: 'foo-zh', content: 'foo-zh' }],
+        ['meta', { name: 'bar', content: 'foobar zh' }],
+      ],
     },
   },
 
