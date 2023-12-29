213cbb6fdaf13aeef8ddff2adc6dbc35e9230ceb
meteorlxy <meteor.lxy@foxmail.com>
2023-12-29 18:00:42 +0800

    test: remove unnecessary wrappers

--- e2e/tests/markdown/links.cy.ts
+++ e2e/tests/markdown/links.cy.ts
@@ -1,37 +1,35 @@
-const E2E_BASE = Cypress.env('E2E_BASE')
+it('should render links and navigate between pages correctly', () => {
+  const E2E_BASE = Cypress.env('E2E_BASE')
 
-describe('markdown > links', () => {
-  it('should render links and navigate between pages correctly', () => {
-    cy.visit('/markdown/links/foo.html')
+  cy.visit('/markdown/links/foo.html')
 
-    cy.get('.e2e-theme-content ul li a')
-      .should('have.length', 2)
-      .first()
-      .should('have.text', 'bar')
-      .click()
+  cy.get('.e2e-theme-content ul li a')
+    .should('have.length', 2)
+    .first()
+    .should('have.text', 'bar')
+    .click()
 
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq(`${E2E_BASE}markdown/links/bar.html`)
-    })
+  cy.location().should((location) => {
+    expect(location.pathname).to.eq(`${E2E_BASE}markdown/links/bar.html`)
+  })
 
-    cy.get('.e2e-theme-content ul li a')
-      .should('have.length', 2)
-      .last()
-      .should('have.text', 'baz')
-      .click()
+  cy.get('.e2e-theme-content ul li a')
+    .should('have.length', 2)
+    .last()
+    .should('have.text', 'baz')
+    .click()
 
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq(`${E2E_BASE}markdown/links/baz.html`)
-    })
+  cy.location().should((location) => {
+    expect(location.pathname).to.eq(`${E2E_BASE}markdown/links/baz.html`)
+  })
 
-    cy.get('.e2e-theme-content ul li a')
-      .should('have.length', 2)
-      .first()
-      .should('have.text', 'foo')
-      .click()
+  cy.get('.e2e-theme-content ul li a')
+    .should('have.length', 2)
+    .first()
+    .should('have.text', 'foo')
+    .click()
 
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq(`${E2E_BASE}markdown/links/foo.html`)
-    })
+  cy.location().should((location) => {
+    expect(location.pathname).to.eq(`${E2E_BASE}markdown/links/foo.html`)
   })
 })
