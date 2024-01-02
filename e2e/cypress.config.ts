9baae5039166e0316f537eb6853150cc220e7866
meteorlxy <meteor.lxy@foxmail.com>
2024-01-02 11:05:46 +0800

    test: increase e2e default timeout

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'http://localhost:9080',
     specPattern: 'tests/**/*.cy.ts',
