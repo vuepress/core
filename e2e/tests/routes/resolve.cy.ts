986e3296031be708b2e826b4c191caf407c395f4
Mr.Hope <mister-hope@outlook.com>
2024-01-24 15:11:19 +0800

    fix: fix e2e

--- e2e/tests/routes/resolve.cy.ts
+++ e2e/tests/routes/resolve.cy.ts
@@ -1,10 +1,10 @@
 it('resolve', () => {
   cy.visit('/routes/resolve.html')
-  const result = { path: '/page-data/meta.html', meta: { a: 0, b: 2, c: 3 } }
 
   cy.get('.e2e-theme-content ul li').each((el) => {
     const data = JSON.parse(/: (\{.*\})\s*$/.exec(el.text())![1])
 
-    expect(data).to.deep.include(result)
+    expect(data.path).to.equal('/page-data/meta.html')
+    expect(data.meta).to.include({ a: 0, b: 2, c: 3 })
   })
 })
