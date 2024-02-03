909ee7c88a0c95bf3f0187c740c3fd8b0186c77b
meteorlxy <meteor.lxy@foxmail.com>
2024-02-04 00:30:40 +0800

    test: add retries for the randomly failing e2e test

--- e2e/tests/markdown/anchors.cy.ts
+++ e2e/tests/markdown/anchors.cy.ts
@@ -1,20 +1,25 @@
-it('should render anchors and navigate correctly', () => {
-  cy.visit('/markdown/anchors.html')
+it(
+  'should render anchors and navigate correctly',
+  // this test is randomly failing on CI, so we need to retry it
+  { retries: 3 },
+  () => {
+    cy.visit('/markdown/anchors.html')
 
-  cy.get('.e2e-theme-content h1')
-    .should('have.attr', 'id', 'title')
-    .should('have.attr', 'tabindex', '-1')
+    cy.get('.e2e-theme-content h1')
+      .should('have.attr', 'id', 'title')
+      .should('have.attr', 'tabindex', '-1')
 
-  cy.get('.e2e-theme-content h1 > a')
-    .should('have.attr', 'class', 'header-anchor')
-    .should('have.attr', 'href', '#title')
-    .click()
+    cy.get('.e2e-theme-content h1 > a')
+      .should('have.attr', 'class', 'header-anchor')
+      .should('have.attr', 'href', '#title')
+      .click()
 
-  cy.hash().should('eq', '#title')
+    cy.hash().should('eq', '#title')
 
-  cy.get('#anchor-1-1 > a')
-    .should('have.attr', 'class', 'header-anchor')
-    .click()
+    cy.get('#anchor-1-1 > a')
+      .should('have.attr', 'class', 'header-anchor')
+      .click()
 
-  cy.hash().should('eq', '#anchor-1-1')
-})
+    cy.hash().should('eq', '#anchor-1-1')
+  },
+)
