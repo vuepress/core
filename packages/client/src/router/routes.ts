a29029563ec9902e79a9a8a3f6d24bbd7b119155
meteorlxy <meteor.lxy@foxmail.com>
2024-02-03 23:34:58 +0800

    fix(client): fix routes types

--- packages/client/src/router/routes.ts
+++ packages/client/src/router/routes.ts
@@ -2,17 +2,9 @@ import {
   redirects as redirectsRaw,
   routes as routesRaw,
 } from '@internal/routes'
-import type {
-  PageChunk,
-  PageMetaDefault,
-  Redirects,
-  Route,
-  Routes,
-} from '@internal/routes'
 import { shallowRef } from 'vue'
 import type { Ref } from 'vue'
-
-export type { PageMetaDefault, PageChunk, Redirects, Route, Routes }
+import type { Redirects, Routes } from '../types/index.js'
 
 /**
  * Global redirects ref
@@ -26,10 +18,10 @@ export const routes: Ref<Routes> = shallowRef(routesRaw)
 
 if (__VUEPRESS_DEV__ && (import.meta.webpackHot || import.meta.hot)) {
   // reuse vue HMR runtime
-  __VUE_HMR_RUNTIME__.updateRoutes = (data: Routes) => {
-    routes.value = data
-  }
   __VUE_HMR_RUNTIME__.updateRedirects = (data: Redirects) => {
     redirects.value = data
+  }
+  __VUE_HMR_RUNTIME__.updateRoutes = (data: Routes) => {
+    routes.value = data
   }
 }
