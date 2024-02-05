6edc2d243eee04f9dc1001f95cbed4d1a12c1dcd
meteorlxy <meteor.lxy@foxmail.com>
2024-02-05 13:37:59 +0800

    chore(client): add comments for ClientOnly

--- packages/client/src/components/ClientOnly.ts
+++ packages/client/src/components/ClientOnly.ts
@@ -1,5 +1,8 @@
 import { defineComponent, onMounted, ref } from 'vue'
 
+/**
+ * Wrapper component that only renders its content on the client side and skips server side rendering
+ */
 export const ClientOnly = defineComponent({
   name: 'ClientOnly',
 
