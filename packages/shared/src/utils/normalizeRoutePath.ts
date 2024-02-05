2d6e9440de216a84b01990ea1fb39455db315aec
Mr.Hope <mister-hope@outlook.com>
2024-02-05 21:57:10 +0800

    fix(shared): fix normalizeRoutePath

--- packages/shared/src/utils/normalizeRoutePath.ts
+++ packages/shared/src/utils/normalizeRoutePath.ts
@@ -1,13 +1,23 @@
+const HASH_REGEXP = /#.*$/u
+
 /**
  * Normalize the given path to the final route path
  */
-export const normalizeRoutePath = (path: string): string => {
-  if (!path || path.endsWith('/')) {
+export const normalizeRoutePath = (
+  path: string,
+  preserveHash = true,
+): string => {
+  if (!path) {
     return path
   }
 
-  // convert README.md to index.html
-  let routePath = path.replace(/(^|\/)README.md$/i, '$1index.html')
+  const hash = preserveHash ? HASH_REGEXP.exec(path)?.[0] || '' : ''
+
+  let routePath = path
+    // remove hash
+    .replace(HASH_REGEXP, '')
+    // convert README.md to index.html
+    .replace(/(^|\/)README.md$/i, '$1index.html')
 
   // convert /foo/bar.md to /foo/bar.html
   if (routePath.endsWith('.md')) {
@@ -15,14 +25,14 @@ export const normalizeRoutePath = (path: string): string => {
   }
 
   // convert /foo/bar to /foo/bar.html
-  if (!routePath.endsWith('.html')) {
+  if (routePath && !routePath.endsWith('.html') && !routePath.endsWith('/')) {
     routePath = routePath + '.html'
   }
 
   // convert /foo/index.html to /foo/
   if (routePath.endsWith('/index.html')) {
-    return routePath.substring(0, routePath.length - 10)
+    return routePath.substring(0, routePath.length - 10) + hash
   }
 
-  return routePath
+  return routePath + hash
 }
