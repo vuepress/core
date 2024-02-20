651dac5a26ed39f4937c5d5c492cad5cb09718a0
meteorlxy <meteor.lxy@foxmail.com>
2024-02-20 18:05:52 +0800

    refactor: move defineClientConfig back to client package

---
+++ packages/client/src/utils/defineClientConfig.ts
@@ -0,0 +1,8 @@
+import type { ClientConfig } from '../types/index.js'
+
+/**
+ * A helper function to help you define vuepress client config file
+ */
+export const defineClientConfig = (
+  clientConfig: ClientConfig = {},
+): ClientConfig => clientConfig
--- packages/vuepress/src/client.ts
+++ packages/vuepress/src/client.ts
@@ -1,10 +1,1 @@
-import type { ClientConfig } from '@vuepress/client'
-
-/**
- * A helper function to help you define vuepress client config file
- */
-export const defineClientConfig = (
-  clientConfig: ClientConfig = {},
-): ClientConfig => clientConfig
-
 export * from '@vuepress/client'
