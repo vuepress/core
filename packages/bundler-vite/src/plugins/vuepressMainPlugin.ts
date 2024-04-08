b752f9c79a693dedeb0c352fd94979f480bad2e7
Mr.Hope <mister-hope@outlook.com>
2024-04-08 10:52:55 +0800

    feat: enable hydration mismatch details in debug mode (#1530)

--- packages/bundler-vite/src/plugins/vuepressMainPlugin.ts
+++ packages/bundler-vite/src/plugins/vuepressMainPlugin.ts
@@ -209,7 +209,7 @@ const resolveDefine = async ({
     // enable options API by default
     __VUE_OPTIONS_API__: JSON.stringify(true),
     __VUE_PROD_DEVTOOLS__: JSON.stringify(app.env.isDebug),
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(app.env.isDebug),
   }
 
   // override vite built-in define config in debug mode
