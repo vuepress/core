e4a72da0edda217c5892adfb38f50a252bf8f72a
meteorlxy <meteor.lxy@foxmail.com>
2024-05-17 16:45:17 +0800

    test(e2e): add test cases for css modules

---
+++ e2e/docs/.vuepress/theme/client/layouts/CssModulesLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import styles from '../styles/styles.module.css?module'
+import variables from '../styles/variables.module.scss?module'
+</script>
+
+<template>
+  <div class="e2e-theme-css-modules-layout">
+    <main class="e2e-theme-css-modules-layout-content">
+      <div id="e2e-theme-css-modules-scss">{{ variables.fooScss }}</div>
+      <div id="e2e-theme-css-modules-css" :class="styles.greenText">
+        CSS modules green text
+      </div>
+      <Content />
+    </main>
+  </div>
+</template>
