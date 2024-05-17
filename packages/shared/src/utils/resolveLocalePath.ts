0ac082001cf20913b20226c245daf20f1ca9dec6
Mr.Hope <mister-hope@outlook.com>
2024-05-17 14:22:03 +0800

    fix(core): encode localeConfig key

--- packages/shared/src/utils/resolveLocalePath.ts
+++ packages/shared/src/utils/resolveLocalePath.ts
@@ -7,13 +7,15 @@ export const resolveLocalePath = (
   locales: LocaleConfig,
   routePath: string,
 ): string => {
-  const localePaths = Object.keys(locales).sort((a, b) => {
-    const levelDelta = b.split('/').length - a.split('/').length
-    if (levelDelta !== 0) {
-      return levelDelta
-    }
-    return b.length - a.length
-  })
+  const localePaths = Object.keys(locales)
+    .map(encodeURI)
+    .sort((a, b) => {
+      const levelDelta = b.split('/').length - a.split('/').length
+      if (levelDelta !== 0) {
+        return levelDelta
+      }
+      return b.length - a.length
+    })
 
   for (const localePath of localePaths) {
     if (routePath.startsWith(localePath)) {
