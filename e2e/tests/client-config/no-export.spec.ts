a69f4dd7d10d61a216962f6f49e450bb885c8ec8
Mister-Hope <mister-hope@outlook.com>
2024-05-22 14:56:35 +0800

    chore: tweaks

--- e2e/tests/client-config/no-export.spec.ts
+++ e2e/tests/client-config/no-export.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test('should import styles in client config file not exports', async ({
   page,
 }) => {
-  await page.goto('markdown/anchor.html', { waitUntil: 'domcontentloaded' })
+  await page.goto('markdown/anchors.html', { waitUntil: 'domcontentloaded' })
 
   await expect(page.locator('#title')).toHaveCSS('font-size', '32px')
 })
