64f021505389c97a818cc57c2c5926f7bca3fdeb
meteorlxy <meteor.lxy@foxmail.com>
2024-09-03 23:24:51 +0800

    chore(client): add comments for data-allow-mismatch usage

--- packages/client/src/components/ClientOnly.ts
+++ packages/client/src/components/ClientOnly.ts
@@ -2,6 +2,10 @@ import { defineComponent, onMounted, ref } from 'vue'
 
 /**
  * Wrapper component that only renders its content on the client side and skips server side rendering
+ *
+ * Since vue 3.5, you can try the new `data-allow-mismatch` attribute instead of `<ClientOnly>` component in some cases to avoid hydration mismatch.
+ *
+ * @see https://blog.vuejs.org/posts/vue-3-5#data-allow-mismatch
  */
 export const ClientOnly = defineComponent({
   name: 'ClientOnly',
