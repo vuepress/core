7d7054a65d2e835883302c84e02cdb27757a40d7
Mister-Hope <mister-hope@outlook.com>
2025-02-21 14:24:38 +0800

    feat(core): set cleanUrl for linksPlugin

--- packages/core/src/app/resolveAppMarkdown.ts
+++ packages/core/src/app/resolveAppMarkdown.ts
@@ -8,6 +8,14 @@ import type { App } from '../types/index.js'
  * @internal
  */
 export const resolveAppMarkdown = async (app: App): Promise<Markdown> => {
+  app.options.markdown.links ??= {}
+
+  // links plugin is not disabled
+  if (app.options.markdown.links !== false) {
+    // set the cleanUrl option
+    app.options.markdown.links.cleanUrl = app.options.route.cleanUrl
+  }
+
   // plugin hook: extendsMarkdownOptions
   await app.pluginApi.hooks.extendsMarkdownOptions.process(
     app.options.markdown,
