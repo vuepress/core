a91367a3a81a7e411a36fa4e79f7f59f5a530ef3
meteorlxy <meteor.lxy@foxmail.com>
2025-02-23 23:06:48 +0800

    test: update e2e

--- e2e/tests/composables/on-content-updated.spec.ts
+++ e2e/tests/composables/on-content-updated.spec.ts
@@ -1,17 +1,20 @@
 import { expect, test } from '@playwright/test'
-import { BUNDLER, IS_DEV } from '../utils/env'
-import { readSourceMarkdown, writeSourceMarkdown } from '../utils/source'
+import { BUNDLER, IS_DEV } from '../../utils/env'
+import { readSourceMarkdown, writeSourceMarkdown } from '../../utils/source'
 
 const updateMarkdownContent = async (): Promise<void> => {
-  const content = await readSourceMarkdown('content-hooks/content.md')
+  const content = await readSourceMarkdown('composables/on-content-updated.md')
   await writeSourceMarkdown(
-    'content-hooks/content.md',
+    'composables/on-content-updated.md',
     `${content}\n\nUpdated content`,
   )
 }
 
 const restoreMarkdownContent = async (): Promise<void> => {
-  await writeSourceMarkdown('content-hooks/content.md', '## title\n\ncontent\n')
+  await writeSourceMarkdown(
+    'composables/on-content-updated.md',
+    '## title\n\ncontent\n',
+  )
 }
 
 test.afterAll(async () => {
@@ -19,23 +22,23 @@ test.afterAll(async () => {
 })
 
 test('should call content hook on mounted', async ({ page }) => {
-  await page.goto('content-hooks/content.html')
+  await page.goto('composables/on-content-updated.html')
   const mountedLocator = page.locator(
     '.markdown-content-hooks .markdown-content-mounted',
   )
   await expect(mountedLocator).toHaveText(
-    'mounted: /content-hooks/content.html 1',
+    'mounted: /composables/on-content-updated.html 1',
   )
 
   // update content but mounted hook should not be called twice
   await updateMarkdownContent()
   await expect(mountedLocator).toHaveText(
-    'mounted: /content-hooks/content.html 1',
+    'mounted: /composables/on-content-updated.html 1',
   )
 })
 
 test('should call content hook on beforeUnmount', async ({ page }) => {
-  await page.goto('content-hooks/content.html')
+  await page.goto('composables/on-content-updated.html')
 
   const beforeUnmountLocator = page.locator(
     '.markdown-content-hooks .markdown-content-beforeUnmount',
@@ -52,7 +55,7 @@ test('should call content hook on beforeUnmount', async ({ page }) => {
  */
 if (IS_DEV && BUNDLER !== 'vite') {
   test('should call content hook on updated', async ({ page }) => {
-    await page.goto('content-hooks/content.html')
+    await page.goto('composables/on-content-updated.html')
     const updatedLocator = page.locator(
       '.markdown-content-hooks .markdown-content-updated',
     )
