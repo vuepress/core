863ca1959438661fc96ea896edef11132ad5279d
CritasWang <critas@outlook.com>
2025-04-22 15:23:50 +0800

    fix: e2e error

--- e2e/tests/router/navigate-by-router.spec.ts
+++ e2e/tests/router/navigate-by-router.spec.ts
@@ -31,12 +31,12 @@ test('should preserve query and hash', async ({ page }) => {
 
 test('should preserve hash', async ({ page }) => {
   await page.locator('#not-found-with-hash').click()
-  await expect(page).toHaveURL(`${BASE}404.html#404`)
+  await expect(page).toHaveURL(`${BASE}404.html#_404`)
   await expect(page.locator('#notfound-h2')).toHaveText('NotFound H2')
 })
 
 test('should preserve hash and query', async ({ page }) => {
   await page.locator('#not-found-with-hash-and-query').click()
-  await expect(page).toHaveURL(`${BASE}404.html#404?notFound=true`)
+  await expect(page).toHaveURL(`${BASE}404.html#_404?notFound=true`)
   await expect(page.locator('#notfound-h2')).toHaveText('NotFound H2')
 })
