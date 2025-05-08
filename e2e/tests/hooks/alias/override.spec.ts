d0481777b68d10450a0d41a4231c8d4eaaab753c
meteorlxy <meteor.lxy@foxmail.com>
2025-05-08 23:12:57 +0800

    chore: refine e2e structure

--- e2e/tests/hooks/alias/override.spec.ts
+++ e2e/tests/hooks/alias/override.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
-test('longer alias shall override shorter ones', async ({ page }) => {
-  await page.goto('alias/override.html')
+test('longer aliases should override shorter ones', async ({ page }) => {
+  await page.goto('hooks/alias/override.html')
   await expect(page.locator('#a')).toHaveText('dir2 > a')
   await expect(page.locator('#b')).toHaveText('dir2 > b')
 })
