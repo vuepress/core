f536347932210a8c3c35ea05dc99f468c42b1304
meteorlxy <meteor.lxy@foxmail.com>
2025-08-05 22:51:08 +0800

    feat(cli): upgrade chokidar to v4 (close #1605)

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,10 +3,11 @@ import fs from 'fs-extra'
 import hash from 'hash-sum'
 import ora from 'ora'
 import colors from 'picocolors'
+import picomatch from 'picomatch'
 import * as tinyglobby from 'tinyglobby'
 import path from 'upath'
 
-export { debug, colors, fs, hash, ora, path, tinyglobby }
+export { debug, colors, fs, hash, ora, path, picomatch, tinyglobby }
 
 export * from './console/index.js'
 export * from './module/index.js'
