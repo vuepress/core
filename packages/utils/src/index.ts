5eea31546e5442f2de0a3eb3872a9208f505106e
Xinyu Liu <meteor.lxy@foxmail.com>
2025-08-06 13:34:44 +0800

    refactor(utils): migrate from globby to tinyglobby (#1660)
    BREAKING CHANGE: the `globby` export has been removed, please use `tinyglobby` instead

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,12 +1,12 @@
 import debug from 'debug'
 import fs from 'fs-extra'
-import { globby } from 'globby'
 import hash from 'hash-sum'
 import ora from 'ora'
 import colors from 'picocolors'
+import * as tinyglobby from 'tinyglobby'
 import path from 'upath'
 
-export { debug, colors, fs, globby, hash, ora, path }
+export { debug, colors, fs, hash, ora, path, tinyglobby }
 
 export * from './console/index.js'
 export * from './module/index.js'
