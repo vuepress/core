4d9ef001be12deefd4bfa951208879a0b87ff9f2
meteorlxy <meteor.lxy@foxmail.com>
2026-02-09 23:32:36 +0800

    style: format code

--- packages/client/src/devtools/types.ts
+++ packages/client/src/devtools/types.ts
@@ -4,7 +4,9 @@ import type { ClientData } from '../types/index.js'
 export type ClientDataKey = keyof ClientData
 export type ClientDataValue = ClientData[ClientDataKey]
 
-export interface InspectorNodeConfig
-  extends Pick<CustomInspectorNode, 'id' | 'label'> {
+export interface InspectorNodeConfig extends Pick<
+  CustomInspectorNode,
+  'id' | 'label'
+> {
   keys: ClientDataKey[]
 }
