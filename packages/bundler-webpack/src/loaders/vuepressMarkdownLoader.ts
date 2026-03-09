f34b1c61f2f51f4121caa83c22c8282dd1fa8707
meteorlxy <meteor.lxy@foxmail.com>
2026-03-10 00:27:34 +0800

    test: try fixing e2e

--- packages/bundler-webpack/src/loaders/vuepressMarkdownLoader.ts
+++ packages/bundler-webpack/src/loaders/vuepressMarkdownLoader.ts
@@ -11,13 +11,19 @@ export interface VuepressMarkdownLoaderOptions {
 export const vuepressMarkdownLoader: LoaderDefinitionFunction<VuepressMarkdownLoaderOptions> =
   async function vuepressMarkdownLoader(source) {
     // import esm dependencies
-    const { createPage, renderPageToVue } = await import('@vuepress/core')
+    const [{ createPage, renderPageToVue }, { path }] = await Promise.all([
+      import('@vuepress/core'),
+      import('@vuepress/utils'),
+    ])
 
     // get app instance from loader options
     const { app } = this.getOptions()
 
+    // normalize the resource path to use forward slashes on Windows
+    const filePath = path.normalize(this.resourcePath)
+
     // get the matched page by file path
-    const page = app.pagesMap[this.resourcePath]
+    const page = app.pagesMap[filePath]
 
     // if the page content is not changed, render it to vue component directly
     if (page?.content === source) {
@@ -27,7 +33,7 @@ export const vuepressMarkdownLoader: LoaderDefinitionFunction<VuepressMarkdownLo
     // create a new page with the new content
     const newPage = await createPage(app, {
       content: source,
-      filePath: this.resourcePath,
+      filePath,
     })
     return renderPageToVue(app, newPage)
   }
