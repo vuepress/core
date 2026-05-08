a556147b7adcfe3c0c1b3ea0f2118b3a41738ce7
Mr.Hope <mister-hope@outlook.com>
2026-05-08 11:01:09 +0800

    feat(bundler-webpack): sync build target with vite8

--- packages/bundler-webpack/src/config/resolveEsbuildLoaderOptions.ts
+++ packages/bundler-webpack/src/config/resolveEsbuildLoaderOptions.ts
@@ -8,7 +8,7 @@ export const resolveEsbuildLoaderOptions = (
    *
    * @see https://vite.dev/config/build-options.html#build-target
    */
-  target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
+  target: ['edge111', 'firefox114', 'chrome111', 'safari16.4'],
 
   /**
    * jsx options
