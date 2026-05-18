643aa49b524295baf28105dd46119ea006f2b46e
Mr.Hope <mister-hope@outlook.com>
2026-05-18 09:16:28 +0800

    fix(markdown): adjust default value for absolutePathPrependBase

--- packages/core/src/app/resolveAppMarkdown.ts
+++ packages/core/src/app/resolveAppMarkdown.ts
@@ -15,6 +15,12 @@ export const resolveAppMarkdown = async (app: App): Promise<Markdown> => {
     app,
   )
 
+  if (app.options.markdown.assets !== false) {
+    app.options.markdown.assets ??= {}
+    app.options.markdown.assets.absolutePathPrependBase ??=
+      app.options.bundler.name === '@vuepress/bundler-webpack'
+  }
+
   const markdown = createMarkdown(app.options.markdown)
 
   // plugin hook: extendsMarkdown
