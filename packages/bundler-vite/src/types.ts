d705253ee86a24566c3b42a1dafbf3e128e6a8f4
Mr.Hope <mister-hope@outlook.com>
2026-05-20 10:47:46 +0800

    chore: add comments

--- packages/bundler-vite/src/types.ts
+++ packages/bundler-vite/src/types.ts
@@ -6,8 +6,22 @@ import type { InlineConfig } from 'vite'
  * Options for bundler-vite
  */
 export interface ViteBundlerOptions extends BundlerOptions {
+  /**
+   * Vite options
+   */
   viteOptions?: InlineConfig
+  /**
+   * Options for @vitejs/plugin-vue
+   */
   vuePluginOptions?: VuePluginOptions
+  /**
+   * Modify Vite config
+   *
+   * @param config - Vite config
+   * @param isServer - Whether it is server bundle
+   * @param isBuild - Whether in build mode
+   * @returns Vite config to be merged with the default config
+   */
   configureVite?: (
     config: InlineConfig,
     isServer: boolean,
