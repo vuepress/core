eeb0099c5ac7b5281de4dce9f1354a98e9e8c026
Xinyu Liu <meteor.lxy@foxmail.com>
2026-07-23 18:51:12 +0800

    chore: tweaks

--- packages/bundler-vite/src/index.ts
+++ packages/bundler-vite/src/index.ts
@@ -3,7 +3,6 @@ import { mergeConfig } from 'vite'
 import { viteBundler } from './viteBundler.js'
 
 export type * from './types.js'
-export * from './viteBundler.js'
 
 export { mergeConfig as viteMergeConfig, viteBundler }
 export default viteBundler
--- packages/bundler-webpack/src/index.ts
+++ packages/bundler-webpack/src/index.ts
@@ -3,7 +3,6 @@ import { merge } from 'webpack-merge'
 import { webpackBundler } from './webpackBundler.js'
 
 export type * from './types.js'
-export * from './webpackBundler.js'
 
 export { merge as webpackMergeConfig, webpackBundler }
 export default webpackBundler
